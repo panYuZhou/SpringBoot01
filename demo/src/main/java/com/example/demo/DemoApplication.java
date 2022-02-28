e2e830c8069538833a3e450d1e95b9415338d325
panpan <panpan@qq.com>
2022-02-28 17:48:44 +0800

    third commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,9 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("like");
+        System.out.println("like");
+        System.out.println("like");
+        System.out.println("like");
     }
 
 }
